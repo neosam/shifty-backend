b368bef377346173e1d139d896bb8eed83cce8fa
Simon Goller <neosam+github@posteo.de>
2026-05-16 13:36:23 +0200

    Set version to 2026.136.1-dev

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
 in
   rustPlatform.buildRustPackage {
     pname = "shifty-service";
-    version = "2026.136.0";
+    version = "2026.136.1-dev";
     src = src;
     nativeBuildInputs = with specificPkgs; [curl pkg-config openssl clippy];
     buildInputs = with specificPkgs; [sqlite openssl];
