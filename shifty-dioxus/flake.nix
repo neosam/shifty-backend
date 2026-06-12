83bf8d533033fbccbc0a8588a9848d1cee9fbaf9
Simon Goller <neosam+github@posteo.de>
2026-06-12 15:31:44 +0200

    Set version to 2026.163.2-dev

--- shifty-dioxus/flake.nix
+++ shifty-dioxus/flake.nix
@@ -39,7 +39,7 @@
         };
         frontend-build = pkgs.rustPlatform.buildRustPackage rec {
           pname = "shifty-dioxus";
-          version = "2026.163.1";
+          version = "2026.163.2-dev";
           
           src = ./.;
           
