36da3abf71da57e0be909b80328ea1ffb469d9ae
Simon Goller <neosam+github@posteo.de>
2026-06-28 23:36:24 +0200

    chore: bump version to 2026.179.3-dev

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
         # take the whole repo and `cd shifty-dioxus` in the build phase.
         frontend-build = pkgs.rustPlatform.buildRustPackage {
           pname = "shifty-dioxus";
-          version = "2026.179.2";
+          version = "2026.179.3-dev";
 
           src = ./.;
           setSourceRoot = ''
--- shifty-dioxus/flake.nix
+++ shifty-dioxus/flake.nix
@@ -39,7 +39,7 @@
         };
         frontend-build = pkgs.rustPlatform.buildRustPackage rec {
           pname = "shifty-dioxus";
-          version = "2026.179.2";
+          version = "2026.179.3-dev";
           
           src = ./.;
           
