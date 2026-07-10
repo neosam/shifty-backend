64b63177ce92eda3be883e036383bb1ae02ff1fa
Simon Goller <neosam+github@posteo.de>
2026-07-10 10:35:37 +0200

    Set version to 2.6.0

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
 in
   rustPlatform.buildRustPackage {
     pname = "shifty-service";
-    version = "2.5.1-dev";
+    version = "2.6.0";
     src = src;
     nativeBuildInputs = with specificPkgs; [curl pkg-config openssl clippy];
     buildInputs = with specificPkgs; [sqlite openssl];
--- shifty-dioxus/flake.nix
+++ shifty-dioxus/flake.nix
@@ -39,7 +39,7 @@
         };
         frontend-build = pkgs.rustPlatform.buildRustPackage rec {
           pname = "shifty-dioxus";
-          version = "2.5.1-dev";
+          version = "2.6.0";
           
           src = ./.;
           
